0eab5dcb2f0339485f74fae6917ec94712811865
MISHA <vol.mykhailo@gmail.com>
2025-09-22 01:46:40 +0300

    DZ-5

--- src/Order.java
+++ src/Order.java
@@ -4,9 +4,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Order {
-    private int orderId;           // номер заказа
-    private String userName;       // имя покупателя
-    private double amount;         // сумма заказа
-    private boolean paid;          // статус оплаты
-    private String createdAt;      // дата создания
+    private int orderId;
+    private String userName;
+    private double amount;
+    private boolean paid;
+    private String createdAt;
 }
